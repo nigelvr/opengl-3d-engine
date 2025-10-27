dfd224cbd8f03881c53c4535c600c11b823e0c71
nigelvr <nvredding2@gmail.com>
2025-10-27 13:14:37 -0400

    remove lamp

--- objects.cpp
+++ objects.cpp
@@ -18,19 +18,3 @@ void Cube::draw() {
     shader.installBool("isLamp", false);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 }
-
-// ------------------- Lamp -------------------
-
-Lamp::Lamp(glm::vec3 pos, VertexData& vd, Shader& shader)
-    : Cube(pos, glm::vec3(1.0f, 1.0f, 1.0f), 0.2f, vd, shader) {}
-
-Lamp::Lamp(glm::vec3 pos, glm::vec3 color, VertexData& vd, Shader& shader)
-    : Cube(pos, color, 0.2f, vd, shader) {}
-
-void Lamp::draw() {
-    shader.installM4("model", model());
-    shader.installVec3("lightPos", pos);
-    shader.installVec3("lightColor", color);
-    shader.installBool("isLamp", true);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-}
